f45695f97441439e8414fdf407b18c5d699fe176
AleVeron <alejoveron65@gmail.com>
2021-11-15 22:51:47 -0300

    Prueba Components

---
+++ src/components/NavBar.js
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+const NavBar = () =>  {
+    return (
+        <nav className='nav'>
+            <h1>Future Gamers</h1>
+            <button>Monitores</button>
+            <button>Gabinetes</button>
+            <button>Notebooks</button>
+        </nav>
+    )
+}
+
+export default NavBar;
